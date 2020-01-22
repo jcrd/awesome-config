6443cdac3f8bbdff3a5aad9d9143f95f1fa7bd3c
James Reed <jcrd@tuta.io>
2020-01-22 14:43:33 -0700

    rules: Port to new `ruled.client` interface

--- rules.lua
+++ rules.lua
@@ -1,30 +1,35 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
+local ruled = require("ruled")
 
 local bindings = require("bindings")
 
-awful.rules.rules = {
-    {
-        rule = {},
-        properties = {
-            border_width = beautiful.border_width,
-            border_color = beautiful.border_normal,
-            focus = awful.client.focus.filter,
-            raise = true,
-            size_hints_honor = false,
-            keys = bindings.clientkeys,
-            buttons = bindings.clientbtns,
-            screen = awful.screen.preferred,
-            placement = awful.placement.no_offscreen,
-        },
-    },
-    {
-        rule_any = {
-            class = {
-                "Pinentry",
-                "Lxappearance",
+ruled.client.connect_signal("request::rules", function()
+    ruled.client.append_rules {
+        {
+            id = "global",
+            rule = {},
+            properties = {
+                border_width = beautiful.border_width,
+                border_color = beautiful.border_normal,
+                focus = awful.client.focus.filter,
+                raise = true,
+                size_hints_honor = false,
+                keys = bindings.clientkeys,
+                buttons = bindings.clientbtns,
+                screen = awful.screen.preferred,
+                placement = awful.placement.no_offscreen,
             },
         },
-        properties = {floating = true},
-    },
-}
+        {
+            id = "floating",
+            rule_any = {
+                class = {
+                    "Pinentry",
+                    "Lxappearance",
+                },
+            },
+            properties = {floating = true},
+        },
+    }
+end)
