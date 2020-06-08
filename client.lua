cc1a8e1322fd29dbc63984d0b314880fad216b54
James Reed <jcrd@tuta.io>
2020-06-08 11:07:04 -0600

    client: Redraw titlebar widget when master changes

--- client.lua
+++ client.lua
@@ -5,28 +5,28 @@ local wibox = require("wibox")
 local dovetail = require("awesome-dovetail")
 
 local function filter(c)
-    if dovetail.layout.masterp(c) then
-        return dovetail.widget.tasklist.filter.master
-    else
-        return dovetail.widget.tasklist.filter.stack
+    return function (...)
+        if dovetail.layout.masterp(c) then
+            return dovetail.widget.tasklist.filter.master(...)
+        else
+            return dovetail.widget.tasklist.filter.stack(...)
+        end
     end
 end
 
 client.connect_signal("request::titlebars", function (c)
-    local opts = {
-        size = beautiful.wibar_height,
-    }
-    awful.titlebar(c, opts).widget = awful.widget.tasklist {
+    local widget = awful.widget.tasklist {
         screen = c.screen,
         filter = filter(c),
     }
+    local opts = {
+        size = beautiful.wibar_height,
+    }
+    awful.titlebar(c, opts).widget = widget
 
-    if not c.titlebar_requested then
-        c.titlebar_requested = true
-        c:connect_signal("dovetail::master::update", function (cl)
-            client.emit_signal("request::titlebars", cl)
-        end)
-    end
+    c:connect_signal("dovetail::master::update", function ()
+        widget:emit_signal("widget::redraw_needed")
+    end)
 end)
 
 client.connect_signal("property::floating", function (c)
