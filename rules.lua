e3a4bd66e27ad16ad0163c5b96104bf75071e07c
James Reed <jcrd@tuta.io>
2020-07-06 16:04:49 -0600

    rules: Use new ruled API

--- rules.lua
+++ rules.lua
@@ -1,24 +1,28 @@
 local awful = require("awful")
 local beautiful = require("beautiful")
+local ruled = require("ruled")
 
-awful.rules.rules = {
-    {
+ruled.client.connect_signal("request::rules", function ()
+    ruled.client.append_rule {
+        id = "global",
         rule = {},
         properties = {
-            border_width = beautiful.border_width,
-            border_color = beautiful.border_normal,
             focus = awful.client.focus.filter,
             raise = true,
             size_hints_honor = false,
             screen = awful.screen.preferred,
             placement = awful.placement.no_offscreen,
         },
-    },
-    {
+    }
+
+    ruled.client.append_rule {
+        id = "titlebars",
         rule_any = {type = "normal"},
         properties = {titlebars_enabled = true},
-    },
-    {
+    }
+
+    ruled.client.append_rule {
+        id = "floating",
         rule_any = {
             class = {
                 "Pinentry",
@@ -26,5 +30,15 @@ awful.rules.rules = {
             },
         },
         properties = {floating = true},
-    },
-}
+    }
+end)
+
+ruled.notification.connect_signal("request::rules", function ()
+    ruled.notification.append_rule {
+        rule = {},
+        properties = {
+            screen = awful.screen.preferred,
+            implicit_timeout = 5,
+        },
+    }
+end)
