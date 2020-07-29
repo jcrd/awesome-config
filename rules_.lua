aba1f2527f2dd2bb3de8c7c2a9f1d64d4784af04
James Reed <jcrd@tuta.io>
2020-07-29 09:07:06 -0600

    rules: Make chromium youtube tabs inhibit

--- rules_.lua
+++ rules_.lua
@@ -5,7 +5,7 @@ local ruled = require("ruled")
 local session = require("sessiond_dbus")
 
 local inhibitors = {
-    ["Chromium-freeworld"] = "Netflix",
+    ["Chromium-freeworld"] = {"Netflix", "YouTube"},
 }
 
 local function uninhibit(c)
@@ -51,19 +51,21 @@ ruled.client.connect_signal("request::rules", function ()
         properties = {floating = true},
     }
 
-    for class, name in pairs(inhibitors) do
-        ruled.client.append_rule {
-            id = string.format("%s %s inhibitor", class, name),
-            rule = {class = class},
-            callback = function (c)
-                local t = toggle_inhibit(class, name)
-                c:connect_signal("property::name", t)
-                c:connect_signal("property::active", t)
-                c:connect_signal("property::valid", function ()
-                    if not c.valid then uninhibit(c) end
-                end)
-            end,
-        }
+    for class, names in pairs(inhibitors) do
+        for _, name in ipairs(names) do
+            ruled.client.append_rule {
+                id = string.format("%s %s inhibitor", class, name),
+                rule = {class = class},
+                callback = function (c)
+                    local t = toggle_inhibit(class, name)
+                    c:connect_signal("property::name", t)
+                    c:connect_signal("property::active", t)
+                    c:connect_signal("property::valid", function ()
+                        if not c.valid then uninhibit(c) end
+                    end)
+                end,
+            }
+        end
     end
 end)
 
